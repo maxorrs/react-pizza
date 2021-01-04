709e07475e65e05a19dfa28a6a604ad852729174
Max Strokov <maxorrs@gmail.com>
2021-01-05 01:32:38 +0300

    Настраивает ESLint/Prettier/PropTypes

--- src/components/icons/icon-plus.jsx
+++ src/components/icons/icon-plus.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 /* eslint-disable */
 
-const IconPlus = ({className = ''}) => {
+const IconPlus = ({className}) => {
   return (
     <svg className={className} width="10" height="10" viewBox="0 0 10 10">
       <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" />
